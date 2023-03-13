ed5789e9da3bf173138c15d8ad021503df52b8bd
n4tn <nathanjbird01@gmail.com>
2023-03-13 13:31:45 +0000

    Assignment 2 complete

--- assignments.js
+++ assignments.js
@@ -5,3 +5,11 @@ let population = 67.89;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
